0f354b4e95667d779c54aa68adf219fe689d7d4a
kingastanek <kin.stanek@gmail.com>
2019-12-15 14:46:05 +0100

    Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] =  [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://assets.epicurious.com/photos/5d375cfdae657500091e79e8/6:4/w_620%2Ch_413/TomatoCobbler_RECIPE_071719_7362.jpg')
+  ];
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
